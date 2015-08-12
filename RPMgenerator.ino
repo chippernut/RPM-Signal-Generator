7b54b06797e7d4b8012e32f4a68e081e8a326d2b
chippernut <jon@chippernut.com>
2015-08-12 18:13:02 -0500

    Update RPMgenerator.ino

--- RPMgenerator.ino
+++ RPMgenerator.ino
@@ -1,94 +1,95 @@
+/* 
 
-
-
-/*
-         
    ________    _                                   __ 
   / ____/ /_  (_)___  ____  ___  _________  __  __/ /_
  / /   / __ \/ / __ \/ __ \/ _ \/ ___/ __ \/ / / / __/
 / /___/ / / / / /_/ / /_/ /  __/ /  / / / / /_/ / /_  
 \____/_/ /_/_/ .___/ .___/\___/_/  /_/ /_/\__,_/\__/  
-            /_/   /_/                                 
-Visit us: www.chippernut.com
-Written by:
-Jon Gulbrandson
-12/28/2014
+            /_/   /_/         
+
+
+Visit us: www.chippernut.com 
+Written by: 
+Jon Gulbrandson 
+12/28/2014 
 
 Kudos to: 
-Adafruit (www.adafruit.com)
-Arduino (www.arduino.com)
+Adafruit (www.adafruit.com) 
+Arduino (www.arduino.com) 
 
-RPM SIGNAL GENERATOR
+RPM SIGNAL GENERATOR 
 
 This generates a square wave using the Arduino TONE command. 
 The output is currently confiqured to Digital Pin #5. 
 Wire this pin directly to the input of your SHIFT LIGHT TACH SIGNAL IN 
 No Transistor is needed. 
-                                                                      
-*/
 
-/*******************************************************
-********              CONFIGURATION             ********
-********                                        ********
-********************************************************/
+*/ 
 
-/* INSTRUCTIONS
-Set your RPM lowpoint (RPM_MIN) to the lowest point, do not
-go lower than 1000 RPM.  Set your RPM highpoint (RPM_MAX) to 
-the redline of your vehicle. The SPEED variable is used for 
+/********** CONFIGURATION **********/ 
+
+/* INSTRUCTIONS 
+Set your RPM lowpoint (RPM_MIN) to the lowest point, do not 
+go lower than 2000 RPM. Set your RPM highpoint (RPM_MAX) to 
+the redline of your vehicle. The Accel_Rate variable is used for 
 the speed at which the generator sweeps through the RPM range. 
-A higher SPEED variable will result in a slower speed. Try to keep
-this between (10 - 50).
+A higher Accel_Rate variable will result in a slower speed. Try to keep 
+this between (10 - 50). 
+*/ 
 
-
-*/
-
-float RPM_MIN = 1000;
-float RPM_MAX = 6500;
-int SPEED = 15;
+float RPM_MIN = 2000; 
+float RPM_MAX = 8500; 
+int Accel_Rate = 10; 
 
 
 
-/*******************************************************
-********               DEFINITIONS              ********
-********                                        ********
-********************************************************/
+/********** DEFINITIONS **********/ 
 
 
-boolean accel = false;
-int x;
-
-/*******************************************************
-********                  SETUP                 ********
-********                                        ********
-********************************************************/
+boolean accel = false; 
+int x; 
+long previousMillis = 0;
+unsigned long currentMillis=0;
 
 
-void setup() {
-pinMode(5, OUTPUT);  
-RPM_MIN = (RPM_MIN / 20.6);
-RPM_MAX = (RPM_MAX / 20.6);
-x = RPM_MIN;
-}
+/********** SETUP **********/ 
 
 
-/*******************************************************
-********                MAIN LOOP               ********
-********                                        ********
-********************************************************/
+void setup() { 
+Serial.begin(9600);
+pinMode(5, OUTPUT); 
+RPM_MIN = (RPM_MIN / 60); 
+RPM_MAX = (RPM_MAX / 60); 
+x = RPM_MIN; 
+} 
 
-void loop() {
 
-while (accel==false){
-        x++;
-        tone(5,x);
-        delay(SPEED);
-        if (x>RPM_MAX){accel=true;}
-   }
-while (accel==true){
-         x--;
-        tone(5,x);
-        delay(SPEED); 
-       if (x<RPM_MIN){accel=false;}
-   }
-}
+/********** MAIN LOOP **********/ 
+
+void loop() { 
+
+//tone(5,70);
+
+      while (accel==false){
+         currentMillis = millis();
+        if(currentMillis - previousMillis > Accel_Rate) {
+        previousMillis = currentMillis;         
+        x++; 
+        tone(5,x); 
+        if (x>RPM_MAX){accel=true;} 
+        }
+      }
+       
+      while (accel==true){
+         currentMillis = millis();
+        if(currentMillis - previousMillis > Accel_Rate) {
+        previousMillis = currentMillis;         
+        x--; 
+        tone(5,x); 
+        if (x<RPM_MIN){accel=false;} 
+        }
+      }
+  }
+
+
+   
